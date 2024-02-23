fd28c2dcebd95e00e5d5aa95fcc445efc1a4566f
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-02-23 18:38:25 +0100

    fixe styliing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # 0-create_a_file.pp in /tmp
 
 file { '/tmp/school':
-    ensure => file,
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet',
+  ensure => file,
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
